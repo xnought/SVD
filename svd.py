7789578817875a0e4f8e6d67bf0b3376ba31e126
xnought <bertuccd@oregonstate.edu>
2022-12-06 01:27:32 -0800

    feat: add naive algorithm for full svd using np built in stuff

--- svd.py
+++ svd.py
@@ -3,20 +3,36 @@ import numpy as np
 __all__ = ["svd"]
 
 
+def star(A: np.ndarray):
+    return A.conj().T
+
+
 def svd(A: np.ndarray):
-    S = []
-    U = []
-    V = []
+    (m, n) = A.shape
+
+    #  A' @ A
+    if m > n:
+        S_2, V = np.linalg.eig(star(A) @ A)
+        S = np.sqrt(S_2)
+        U = A @ V / S
+
+    #  A @ A'
+    else:
+        S_2, U = np.linalg.eig(A @ star(A))
+        S = np.sqrt(S_2)
+        V = star(A) @ U / S
+
     return S, U, V
 
 
 def testing():
-    A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
     S, U, V = svd(A)
 
-    print(f"{S = }")
     print(f"{U = }")
+    print(f"{S = }")
     print(f"{V = }")
+    print(f"{U @ np.diag(S) @ star(V) = }")
 
 
 if __name__ == "__main__":
